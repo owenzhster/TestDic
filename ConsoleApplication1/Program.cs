0f7bd4fb15e05b251ca9670e6ed5505cc613d33f
owenzhster <125898286@qq.com>
2016-08-29 16:53:07 +0800

    add

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,60 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+using System;
+
+namespace WMTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String readStr = Console.ReadLine();
+            Console.WriteLine("you have input:" + readStr);
+            outText();
+        }
+
+        static void outText()
+
+        {
+            Program1 p = new Program1();
+            Console.WriteLine(p.outText());
+
+            DynamicAndVar dv = new DynamicAndVar();
+            Console.WriteLine(dv.outText());
+        }
+    }
+
+    class Program1
+    {
+        public String outText()
+        {
+            Object o;
+            o = 100L;
+            o = "200";
+            return "p1.outtext" + Convert.ToString(o.ToString());
+        }
+    }
+
+    class DynamicAndVar
+    {
+        public String outText()
+        {
+            Object o;
+            o = "oStr";
+            dynamic d;
+            d = 100;
+            Program1 p = new Program1();
+            var v = p;
+            Console.WriteLine(o.GetType());
+            Console.WriteLine(d.GetType());
+            Console.WriteLine(v.GetType());
+
+            return "DynamicAndVar.outtext" + ", o:" + o + ", d:" + d + ", v:" + v;
+        }
+    }
+}
+
